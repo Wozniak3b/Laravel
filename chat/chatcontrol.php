5dc078cf380484013bf2ffe70bf387bf25c0411b
Jakub Woźniak <kubawgli@onet.eu>
2024-10-04 14:33:20 +0200

    Some changes

--- chat/chatcontrol.php
+++ chat/chatcontrol.php
@@ -1,11 +1,25 @@
 <?php
-    $database=@mysqli_connect("localhost","root","","chat_db");
+    $database=mysqli_connect("localhost","root","","chat_db");
     if($database->connect_error){
         die("Unfortunately connection with db failed:".$database->connect_error);
     };
 
-    $mess=$_POST['message'];
-    
+    $result=array();
+    $message=isset($_POST['message']) ? $_POST['message'] : null;
+    $name=isset($_POST['name']) ? $_POST['name'] : null;
+
+    if(!empty($message) && !empty($name)){
+        $query="INSERT INTO `chat` (`message`,`user_name`) VALUES ('".$message."','".$name."')";
+        $result=mysqli_query($database,$query);
+    }
 
     mysqli_close($database);
+    
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Methods: PUT, GET, POST");
+    header('Content-Type: application/json; charset=utf-8');
+
+    echo json_encode($result);
 ?>
+
+
